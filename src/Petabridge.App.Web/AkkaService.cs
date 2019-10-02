d0c2e9fa585d690856f3987a739cc9f0cf0921db
Aaron Stannard <aaron@petabridge.com>
2019-10-02 03:42:42 +0100

    implemented Petabridge.App.Web with ASP.NET Core 3.0 (#1)
    * renamed projects and SLNs to Petabridge.App.Web
    
    * reorganized folders
    
    * implemented ASP.NET Core 3.0 app
    
    * fixed docker container
    
    * added port 80 for Docker

---
+++ src/Petabridge.App.Web/AkkaService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Bootstrap.Docker;
+using Akka.Configuration;
+using Microsoft.Extensions.Hosting;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Host;
+using Petabridge.Cmd.Remote;
+
+namespace Petabridge.App.Web
+{
+    public class AkkaService : IHostedService
+    {
+        public static ActorSystem Sys { get; private set; }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            var config = ConfigurationFactory.ParseString(File.ReadAllText("app.conf")).BootstrapFromDocker();
+            Sys = ActorSystem.Create("ClusterSys", config);
+
+            var pbm = PetabridgeCmd.Get(Sys);
+            pbm.RegisterCommandPalette(ClusterCommands.Instance);
+            pbm.RegisterCommandPalette(RemoteCommands.Instance);
+            pbm.Start(); // begin listening for PBM management commands
+
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            return CoordinatedShutdown.Get(Sys).Run(CoordinatedShutdown.ClrExitReason.Instance);
+        }
+    }
+}
---
+++ src/Petabridge.App.Web/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Akka.Actor;
+using Akka.Bootstrap.Docker;
+using Akka.Configuration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Host;
+using Petabridge.Cmd.Remote;
+
+namespace Petabridge.App.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+   
+}
