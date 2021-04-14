af88116e10adaa63226aa13419d457cd364e4e80
Aaron Stannard <aaron@petabridge.com>
2021-04-14 17:20:59 -0500

    moved to IHostedService and DI for all ASP.NET interactivity

--- src/Petabridge.App.Web/AkkaService.cs
+++ src/Petabridge.App.Web/AkkaService.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Akka.Actor;
 using Akka.Bootstrap.Docker;
 using Akka.Configuration;
+using Akka.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Petabridge.Cmd.Cluster;
 using Petabridge.Cmd.Host;
@@ -14,26 +15,69 @@ using Petabridge.Cmd.Remote;
 
 namespace Petabridge.App.Web
 {
-    public class AkkaService : IHostedService
+    public interface IConsoleReporter{
+        void Report(string msg);
+    }
+
+
+    /// <summary>
+    /// <see cref="IHostedService"/> that runs and manages <see cref="ActorSystem"/> in background of application.
+    /// </summary>
+    public class AkkaService : IHostedService, IConsoleReporter
     {
-        public static ActorSystem Sys { get; private set; }
+        private ActorSystem ClusterSystem;
+        private readonly IServiceProvider _serviceProvider;
+
+        public IActorRef ConsoleActor {get; private set;}
+
+        public AkkaService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
 
         public Task StartAsync(CancellationToken cancellationToken)
         {
-            var config = ConfigurationFactory.ParseString(File.ReadAllText("app.conf")).BootstrapFromDocker();
-            Sys = ActorSystem.Create("ClusterSys", config);
+             var config = ConfigurationFactory.ParseString(File.ReadAllText("app.conf")).BootstrapFromDocker();
+             var bootstrap = BootstrapSetup.Create()
+                .WithConfig(config) // load HOCON
+                .WithActorRefProvider(ProviderSelection.Cluster.Instance); // launch Akka.Cluster
 
-            var pbm = PetabridgeCmd.Get(Sys);
+            // N.B. `WithActorRefProvider` isn't actually needed here - the HOCON file already specifies Akka.Cluster
+
+            // enable DI support inside this ActorSystem, if needed
+            var diSetup = ServiceProviderSetup.Create(_serviceProvider);
+
+            // merge this setup (and any others) together into ActorSystemSetup
+            var actorSystemSetup = bootstrap.And(diSetup);
+
+            // start ActorSystem
+            ClusterSystem = ActorSystem.Create("ClusterSys", actorSystemSetup);
+
+            // start Petabridge.Cmd (https://cmd.petabridge.com/)
+            var pbm = PetabridgeCmd.Get(ClusterSystem);
             pbm.RegisterCommandPalette(ClusterCommands.Instance);
             pbm.RegisterCommandPalette(RemoteCommands.Instance);
             pbm.Start(); // begin listening for PBM management commands
 
+            // instantiate actors
+
+            // use the ServiceProvider ActorSystem Extension to start DI'd actors
+            var sp = ServiceProvider.For(ClusterSystem);
+            ConsoleActor = ClusterSystem.ActorOf(Props.Create(() => new ConsoleActor()), "console");
+            
             return Task.CompletedTask;
         }
 
-        public Task StopAsync(CancellationToken cancellationToken)
+        public async Task StopAsync(CancellationToken cancellationToken)
         {
-            return CoordinatedShutdown.Get(Sys).Run(CoordinatedShutdown.ClrExitReason.Instance);
+            // strictly speaking this may not be necessary - terminating the ActorSystem would also work
+            // but this call guarantees that the shutdown of the cluster is graceful regardless
+             await CoordinatedShutdown.Get(ClusterSystem).Run(CoordinatedShutdown.ClrExitReason.Instance);
+        }
+
+        public void Report(string msg)
+        {
+            ConsoleActor.Tell(msg);
         }
     }
 }
