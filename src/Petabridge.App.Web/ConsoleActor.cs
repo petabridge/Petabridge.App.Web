ea1944592e42900d27fedd1727c303360923f761
Aaron Stannard <aaron@petabridge.com>
2022-06-09 10:56:16 -0500

    migrated to Akka.Hosting and .NET 6 file-scoped namespaces
    Also added support for minimal APIs, Akka.NET v1.4.39

---
+++ src/Petabridge.App.Web/ConsoleActor.cs
@@ -0,0 +1,17 @@
+﻿using Akka.Actor;
+using Akka.Event;
+
+namespace Petabridge.App.Web;
+
+public sealed class ConsoleActor : ReceiveActor
+{
+    private readonly ILoggingAdapter _log = Context.GetLogger();
+    public ConsoleActor()
+    {
+        ReceiveAny(_ =>
+        {
+            _log.Info("Received: {0}", _);
+            Sender.Tell(_);
+        });
+    }
+}
--- src/Petabridge.App.Web/Program.cs
+++ src/Petabridge.App.Web/Program.cs
@@ -1,29 +1,66 @@
-﻿using System.IO;
-using System.Threading.Tasks;
+﻿using System.Diagnostics;
 using Akka.Actor;
-using Akka.Bootstrap.Docker;
-using Akka.Configuration;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using Akka.Cluster.Hosting;
+using Akka.Hosting;
+using Akka.Remote.Hosting;
+using Petabridge.App.Web;
 using Petabridge.Cmd.Cluster;
+using Petabridge.Cmd.Cluster.Sharding;
 using Petabridge.Cmd.Host;
 using Petabridge.Cmd.Remote;
 
-namespace Petabridge.App.Web
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") ?? "Development";
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
-   
-}
+var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables()
+    .AddJsonFile("appsettings.json")
+    .AddJsonFile($"appsettings.{environment}.json");
+
+builder.Logging.ClearProviders().AddConsole();
+
+var akkaConfig = builder.Configuration.GetRequiredSection(nameof(AkkaClusterConfig))
+    .Get<AkkaClusterConfig>();
+
+builder.Services.AddControllers();
+builder.Services.AddAkka(akkaConfig.ActorSystemName, (builder, provider) =>
+{
+    Debug.Assert(akkaConfig.Port != null, "akkaConfig.Port != null");
+    builder.AddHoconFile("app.conf")
+        .WithRemoting(akkaConfig.Hostname, akkaConfig.Port.Value)
+        .WithClustering(new ClusterOptions()
+        {
+            Roles = akkaConfig.Roles?.ToArray() ?? Array.Empty<string>(),
+            SeedNodes = akkaConfig.SeedNodes?.Select(Address.Parse).ToArray() ?? Array.Empty<Address>()
+        })
+        .AddPetabridgeCmd(cmd =>
+        {
+            cmd.RegisterCommandPalette(new RemoteCommands());
+            cmd.RegisterCommandPalette(ClusterCommands.Instance);
+
+            // sharding commands, although the app isn't configured to host any by default
+            cmd.RegisterCommandPalette(ClusterShardingCommands.Instance);
+        })
+        .WithActors((system, registry) =>
+        {
+            var consoleActor = system.ActorOf(Props.Create(() => new ConsoleActor()), "console");
+            registry.Register<ConsoleActor>(consoleActor);
+        });
+});
+
+var app = builder.Build();
+
+app.UseRouting();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    
+    endpoints.MapGet("/", async (HttpContext context, ActorRegistry registry) =>
+    {
+        var reporter = registry.Get<ConsoleActor>();
+        var resp = await reporter.Ask<string>($"hit from {context.TraceIdentifier}", context.RequestAborted); // calls Akka.NET under the covers
+        await context.Response.WriteAsync(resp);
+    });
+});
+
+
+await app.RunAsync();
